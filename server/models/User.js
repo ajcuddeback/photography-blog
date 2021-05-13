8b603e097376c7dcb2ebff8277f62e2c0a07154e
ajcudd <ajcuddeback@gmail.com>
2021-05-12 20:33:35 -0400

    created user schema

---
+++ server/models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        first_name: {
+            type: String,
+            required: true
+        }, 
+        last_name: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email adress']
+        }, 
+        password: {
+            type: String, 
+            required: true
+        }
+    }, 
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
