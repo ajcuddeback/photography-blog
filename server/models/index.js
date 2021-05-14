73a2fb704a3f4972b7ae7e416897a6ad0caa9082
ajcudd <ajcuddeback@gmail.com>
2021-05-14 19:00:33 -0400

    set up index and server to handle routes

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Photo = require('./Photos');
+const Comment = require('./Comments');
+const Tag = require('./Tag');
+
+module.exports = { User, Photo, Comment, Tag };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +17,8 @@ if(process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
