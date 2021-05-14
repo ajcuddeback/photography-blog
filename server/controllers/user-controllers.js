ba48f23d888e2ffb4cfda1ebf52b82f5605b913d
ajcudd <ajcuddeback@gmail.com>
2021-05-14 19:11:12 -0400

    added get single user route

---
+++ server/controllers/user-controllers.js
@@ -0,0 +1,18 @@
+const { User } = require('../models');
+
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    async getSingleUser({ user = null, params }, res) {
+        const foundUser = await User.findOne({
+            $or: [{ _id: user ? user._id : params.id }, { username: params.username }]
+        });
+
+        if(!foundUser) {
+            res.status(400).json({ message: 'No user found at this id!' });
+            return;
+        };
+
+        res.json(foundUser);
+    }
+}
