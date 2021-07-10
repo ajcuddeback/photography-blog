63cb4f88ff6d1cd1308d269f5b61f8dfc295e853
ajcudd <ajcuddeback@gmail.com>
2021-07-10 04:02:20 -0400

    added about page

--- client/src/components/pages/AboutComponent.js
+++ client/src/components/pages/AboutComponent.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import Profile from '../../images/Profile.jpg'
+
+//Dependencies
+import styled from 'styled-components';
 
 const AboutComponent = () => {
     
     return (
-        <>
-            <h1>About</h1>
-        </>
+        <StyledDiv>
+            <img src={Profile} alt="Nikki's Profile Picture" />
+            <h2>Nikki Cuddeback</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi temporibus facilis laboriosam praesentium necessitatibus dolore voluptates cupiditate deleniti, numquam itaque perspiciatis alias nisi. Quibusdam facere sint incidunt dolores, distinctio sed dignissimos, quia dicta soluta repellendus ut, in vel ratione. Officiis placeat sequi corporis, incidunt ab non a quod doloribus laudantium cum quis aliquam. Alias doloremque pariatur repellat laborum iste rem sed magni provident quibusdam unde. Rem, possimus provident. Accusantium veniam adipisci ab. Suscipit officiis quos facilis rem quae sint necessitatibus.</p>
+        </StyledDiv>
     )
 }
 
-export default AboutComponent
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+        margin-top: 3rem;
+        border-radius: 50%;
+        width: 30%;
+    }
+    p {
+        width: 30%;
+        text-align: center;
+    }
+`
+
+export default AboutComponent;
