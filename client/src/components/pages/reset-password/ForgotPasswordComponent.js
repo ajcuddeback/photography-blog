0580ef5bc3245731d5cde36df7743209e526ab4a
ajcudd <ajcuddeback@gmail.com>
2021-07-10 04:37:27 -0400

    added send email code to forgot password comp

--- client/src/components/pages/reset-password/ForgotPasswordComponent.js
+++ client/src/components/pages/reset-password/ForgotPasswordComponent.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { useLocation } from 'react-router';
+import React, { useState } from 'react';
+
+import { sendMail } from '../../../utils/API';
 
 const ForgotPasswordComponent = () => {
 
+    const [email, setEmail] = useState('');
+
+    const handleInputChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await sendMail(email);
+
+            if(!response) {
+                throw new Error("something went wrong");
+            }
+
+            const data = await response.json();
+
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="email">Email: </label>
+                <input onChange={handleInputChange} value={email} type="email" name="email" />
+                <button type="submit">Send my Code</button>
+            </form>
         </>
     )
 
